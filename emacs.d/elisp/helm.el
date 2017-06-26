f19ca0cc423975f1ac56608daeb156b994d06c7f
Raghav Karol <rkarol@basho.com>
2017-06-26 22:54:43 +0200

    Fix for helm-find-buffer-on-elscreen

--- emacs.d/elisp/helm.el
+++ emacs.d/elisp/helm.el
@@ -7,10 +7,25 @@
   (helm-ag--find-file-action candidate 'elscreen-find-file (helm-ag--search-this-file-p)))
 
 ;;; Copied from
+;; --------------------------------------------------------------------------------
 ;; https://github.com/lewang/Emacs-helm/blob/master/helm-elscreen.el
+;; ------------------------------------------------------------------------------
 (defun helm-elscreen-find-file (file)
   (helm-require-or-error 'elscreen 'helm-elscreen-find-file)
   (elscreen-find-file file))
+
+(defun helm-find-buffer-on-elscreen (candidate)
+  "Open buffer in new screen, if marked buffers open all in elscreens."
+  (helm-require-or-error 'elscreen 'helm-find-buffer-on-elscreen)
+  (helm-aif (helm-marked-candidates)
+      (dolist (i it)
+        (let ((target-screen (elscreen-find-screen-by-buffer
+                              (get-buffer i) 'create)))
+          (elscreen-goto target-screen)))
+    (let ((target-screen (elscreen-find-screen-by-buffer
+                          (get-buffer candidate) 'create)))
+      (elscreen-goto target-screen))))
+;; ------------------------------------------------------------------------------
 
 (require 'helm-config)
 (helm-mode 1)                           ; Enable helm globally
