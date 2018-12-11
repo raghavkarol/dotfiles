9576fc2ae71f259d3c873193262d2e9c0e1d89af
Raghav Karol <raghav.karol@alertlogic.com>
2018-12-11 09:45:06 +0100

    Patch elscreen to be 1 based

--- emacs.d/elpa/elscreen-20181009.451/elscreen.el
+++ emacs.d/elpa/elscreen-20181009.451/elscreen.el
@@ -1025,7 +1025,9 @@ is ommitted, current screen will survive."
   (interactive)
   (let ((next-screen (string-to-number (string last-command-event))))
     (if (and (<= 0 next-screen) (<= next-screen 9))
-        (elscreen-goto next-screen))))
+        (cond ((eq next-screen 0) (elscreen-goto 9))
+              ('t (elscreen-goto (- next-screen 1)))))))
+
 (defalias 'elscreen-jump-0 'elscreen-jump)
 (defalias 'elscreen-jump-9 'elscreen-jump)
 
@@ -1519,7 +1521,7 @@ Use \\[toggle-read-only] to permit editing."
                          half-space
                          (propertize
                           (format "%d%s%s%s"
-                                  screen
+                                  (+ screen 1)
                                   (elscreen-status-label screen)
                                   half-space
                                   (elscreen-tab-escape-%
