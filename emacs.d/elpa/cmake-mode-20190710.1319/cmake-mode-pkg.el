e8c99801eea1426c699241d293519c03387f8c3b
Raghav Karol <raghav.karol@alertlogic.com>
2019-08-22 21:45:18 +0200

    Update packages

---
+++ emacs.d/elpa/cmake-mode-20190710.1319/cmake-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "cmake-mode" "20190710.1319" "major-mode for editing CMake sources" '((emacs "24.1")) :commit "473f288e9703ad49e6be8f3fc8639e0bbb79cc9b")
