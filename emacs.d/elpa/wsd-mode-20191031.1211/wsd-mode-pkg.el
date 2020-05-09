69eac7b84c8a3159135fdd4caee5057789f3c970
Raghav Karol <raghav.karol@alertlogic.com>
2020-05-09 13:58:58 +0200

    Update packages

---
+++ emacs.d/elpa/wsd-mode-20191031.1211/wsd-mode-pkg.el
@@ -0,0 +1,10 @@
+(define-package "wsd-mode" "20191031.1211" "Emacs major-mode for www.websequencediagrams.com" 'nil :keywords
+  '("wsd" "diagrams" "design" "process" "modelling" "uml")
+  :authors
+  '(("Jostein Kjønigsen" . "jostein@gmail.com"))
+  :maintainer
+  '("Jostein Kjønigsen" . "jostein@gmail.com")
+  :url "https://github.com/josteink/wsd-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
