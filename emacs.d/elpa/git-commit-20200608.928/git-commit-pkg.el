e16e4b72d18acf465f92a9d59c2372e2f2e0c0bf
Raghav Karol <raghav.karol@alertlogic.com>
2020-06-13 11:47:51 +0200

    Upgrade packages

---
+++ emacs.d/elpa/git-commit-20200608.928/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-commit" "20200608.928" "Edit Git commit messages" '((emacs "25.1") (dash "20200524") (transient "20200601") (with-editor "20200522")) :commit "a1f8faaaebc5860e2f290cbcea76e4c114342a74" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
