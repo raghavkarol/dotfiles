cdca6510a3da7597fabc5bf4b58cfc556771ac4b
Raghav Karol <raghav.karol@alertlogic.com>
2022-06-16 09:35:01 +0200

    Update packages

--- emacs.d/elpa/go-dlv-20200713.1202/go-dlv-autoloads.el
+++
@@ -1,45 +0,0 @@
-;;; go-dlv-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory #$) (car load-path))))
-
-
-;;;### (autoloads nil "go-dlv" "../../../../../../.emacs.d/elpa/go-dlv-20200713.1202/go-dlv.el"
-;;;;;;  "3069c29f5c5cc0cf16fa3f0a65fe08f4")
-;;; Generated autoloads from ../../../../../../.emacs.d/elpa/go-dlv-20200713.1202/go-dlv.el
-
-(autoload 'dlv "go-dlv" "\
-Run dlv on program FILE in buffer `*gud-FILE*'.
-The directory containing FILE becomes the initial working directory
-and source-file directory for your debugger.
-
-\(fn COMMAND-LINE)" t nil)
-
-(autoload 'dlv-current-func "go-dlv" "\
-Debug the current program or test stopping at the beginning of the current function." t nil)
-
-;;;### (autoloads "actual autoloads are elsewhere" "go-dlv" "../../../../../../.emacs.d/elpa/go-dlv-20200713.1202/go-dlv.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from ../../../../../../.emacs.d/elpa/go-dlv-20200713.1202/go-dlv.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "go-dlv" '("go-dlv-marker-" "gud-dlv-command-name")))
-
-;;;***
-
-;;;***
-
-;;;### (autoloads nil nil ("../../../../../../.emacs.d/elpa/go-dlv-20200713.1202/go-dlv-autoloads.el"
-;;;;;;  "../../../../../../.emacs.d/elpa/go-dlv-20200713.1202/go-dlv.el")
-;;;;;;  (0 0 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; go-dlv-autoloads.el ends here
