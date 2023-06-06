8cb73e6348a929c0cb799a42de093a70b09cc1f0
Raghav Karol <raghav.karol@alertlogic.com>
2023-06-06 13:37:05 +0200

    Update packages

---
+++ emacs.d/elpa/wgrep-helm-20230202.315/wgrep-helm-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from wgrep-helm.el  -*- no-byte-compile: t -*-
+(define-package "wgrep-helm" "20230202.315" "Writable helm-grep-mode buffer" '((emacs "25.1") (wgrep "3.0.0")) :commit "ff3cf631b6842432daa59bf604049ca916cce73b" :authors '(("Masahiro Hayashi" . "mhayashi1120@gmail.com")) :maintainers '(("Masahiro Hayashi" . "mhayashi1120@gmail.com")) :maintainer '("Masahiro Hayashi" . "mhayashi1120@gmail.com") :keywords '("grep" "edit" "extensions") :url "http://github.com/mhayashi1120/Emacs-wgrep/raw/master/wgrep-helm.el")
