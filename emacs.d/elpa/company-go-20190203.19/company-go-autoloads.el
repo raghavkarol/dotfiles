77d3e1e21450c2a5a994756a84c3d9a4ce95730c
Raghav Karol <raghav@macbooknvilogic.lan>
2023-11-26 17:31:48 +0100

    Auto-update of packages

--- emacs.d/elpa/company-go-20190203.19/company-go-autoloads.el
+++
@@ -1,40 +0,0 @@
-;;; company-go-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory #$) (car load-path))))
-
-
-;;;### (autoloads nil "company-go" "../../../../../../.emacs.d/elpa/company-go-20190203.19/company-go.el"
-;;;;;;  "33ef1c0a1b3df354b2937d1367f79785")
-;;; Generated autoloads from ../../../../../../.emacs.d/elpa/company-go-20190203.19/company-go.el
-
-(autoload 'company-go "company-go" "\
-
-
-\(fn COMMAND &optional ARG &rest IGNORED)" t nil)
-
-;;;### (autoloads "actual autoloads are elsewhere" "company-go" "../../../../../../.emacs.d/elpa/company-go-20190203.19/company-go.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from ../../../../../../.emacs.d/elpa/company-go-20190203.19/company-go.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "company-go" '("company-go-")))
-
-;;;***
-
-;;;***
-
-;;;### (autoloads nil nil ("../../../../../../.emacs.d/elpa/company-go-20190203.19/company-go-autoloads.el"
-;;;;;;  "../../../../../../.emacs.d/elpa/company-go-20190203.19/company-go.el")
-;;;;;;  (0 0 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; company-go-autoloads.el ends here
