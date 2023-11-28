a193336fdf6fe6bc075b98d939ff48685c71f728
Raghav Karol <raghav@macbook-anvilogic.local>
2023-11-28 13:42:20 +0100

    Update packages

---
+++ emacs.d/elpa/base16-theme-3.2/base16-theme-pkg.el
@@ -0,0 +1,9 @@
+(define-package "base16-theme" "3.2" "Collection of themes built on combinations of 16 base colors" 'nil :commit "a01e1fe184dbcd5d0c7ed706ae144eb9299cd417" :authors
+  '(("Kaleb Elwert" . "belak@coded.io")
+    ("Neil Bhakta"))
+  :maintainer
+  '("Kaleb Elwert" . "belak@coded.io")
+  :url "https://github.com/tinted-theming/base16-emacs")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
