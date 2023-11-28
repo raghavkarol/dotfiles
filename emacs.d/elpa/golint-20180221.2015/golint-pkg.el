cbe8ae0edd4c966cd3da3508de592508b338f31c
Raghav Karol <raghav@macbook-anvilogic.local>
2023-11-28 15:13:02 +0100

    Update packages

--- emacs.d/elpa/golint-20180221.2015/golint-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /Users/rkarol-admin/.emacs.d/elpa/golint-20180221.2015/golint.el  -*- no-byte-compile: t -*-
-(define-package "golint" "20180221.2015" "lint for the Go source code" 'nil :commit "83fdc39ff7b56453e3793356bcff3070b9b96445" :url "https://github.com/golang/lint")
